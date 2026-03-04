f751cbfb1d0704ee13a06490dfea86366fc28834
Denis <bezsaluy@gmail.com>
2026-03-04 10:48:56 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 'use strict'
+
+// 1
+
+function createLogger(prefix) {
+
+    return function(message) {
+        
+        console.log(`${prefix}: ${message}`)
+
+    }
+
+}
+
+const authLogger = createLogger('AUTH');
+
+const apiLogger = createLogger('API');
+
+authLogger('User logged in');
+
+apiLogger('Request failed');
+
+// 2
+
+function createLimiter(limit) {
+
+    if(typeof limit !== 'number' || Number.isNaN(limit) || limit < 1) {
+
+        console.log('Wrong value')
+
+    }
+    else{
+
+    let count = 0
+
+    return function() {
+
+        if (count < limit) {
+            count++;
+            return "Ok";
+        }
+
+        return "Error";
+
+    }
+    
+}
+}
+
+const limited = createLimiter(2);
+
+console.log(limited());
+
+console.log(limited());
+
+console.log(limited());
